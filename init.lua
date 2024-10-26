24272493d3d7c4b0f6fdee70acdd2c1f458674e3
Christopher LIU <chrisckeyliu@gmail.com>
2024-10-26 14:51:18 +0800

    [Upd] Min

--- init.lua
+++ init.lua
@@ -15,3 +15,28 @@ lspconfig.clangd.setup({
     "--offset-encoding=utf-16",
   },
 })
+
+---- p4 analyzer
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.p4",
+  callback = function()
+    vim.bo.filetype = "p4"
+  end,
+})
+
+-- local configs = require("lspconfig.configs")
+-- if not configs.p4analyzer then
+--   configs.p4analyzer = {
+--     default_config = {
+--       cmd = {
+--         "p4analyzer",
+--         "--stdio",
+--         "--logpath",
+--         "/home/christopher/p4-logs/",
+--       },
+--       root_dir = lspconfig.util.root_pattern(".git"),
+--       filetypes = { "p4" },
+--     },
+--   }
+-- end
+-- lspconfig.p4analyzer.setup({})
